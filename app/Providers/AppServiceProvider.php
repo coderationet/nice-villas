2ef7a986e6564e1ebb3fa890c1c9ef413813bb8a
Gokhan <dinatopya03@gmail.com>
2023-08-08 21:30:53 +0300

    ignore-fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Vite;
+//use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,8 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::useScriptTagAttributes([
-            'defer' => true, // Specify an attribute without a value...
-        ]);
+//        Vite::useScriptTagAttributes([
+//            'defer' => true, // Specify an attribute without a value...
+//        ]);
     }
 }
